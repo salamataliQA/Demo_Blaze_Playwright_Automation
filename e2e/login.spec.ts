1f4bbce0231b20834635152be90d2b0638d522e6
salamataliQA <salamatali2750@gmail.com>
2025-10-19 16:34:09 +0500

    Update login spec
    complete Sign up flow

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -4,11 +4,23 @@ test('Verify that user can able to login', async ({ page }) => {
     await page.goto('https://www.demoblaze.com/')
     await expect(page).toHaveTitle('STORE')
 
-    const signUpbtn = page.getByRole('link', { name: 'Sign up' }).filter({ hasText: 'Sign Up' })
-    await expect(signUpbtn).toBeVisible()
-    await signUpbtn.click()
+    const signUpLink = page.getByRole('link', { name: 'Sign up' }).filter({ hasText: 'Sign Up' })
+    await expect(signUpLink).toBeVisible()
+    await signUpLink.click()
 
     const signUpmodal = page.getByRole('heading', { name: 'Sign up' })
     await expect(signUpmodal).toContainText('Sign up')
     await signUpmodal.click()
+
+    const userNameSignUp = page.getByRole('textbox', { name: 'Username:' }).fill('Test123')
+    // await expect(userNameSignUp).toBeVisible()
+    const passwordSignUp = page.getByRole('textbox', { name: 'Password:' }).fill('123445566')
+
+    const signUpbtn = page.getByRole('button', { name: 'Sign up' })
+    await expect(signUpbtn).toBeVisible()
+    await signUpbtn.click()
+
+    const closeSignUpModal = page.getByLabel('Sign up').getByText('Close')
+    await expect(closeSignUpModal).toBeVisible()
+    await closeSignUpModal.click()
 })
